35bc9228f69dfaa4b6780d56e6368c14c5f9933b
dm1\u12797 <z.ghavidel@dm1.com>
2020-11-09 14:12:56 +0330

    Add signalR to monitoring.

--- ClientApp/src/pages/monitoring/monitoring.jsx
+++ ClientApp/src/pages/monitoring/monitoring.jsx
@@ -1,39 +1,71 @@
-import React, {Component} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import userService from "../../services/userService";
 import Cluster from "./cluster";
+import { HubConnectionBuilder } from '@microsoft/signalr';
 
+const Monitoring = ()=>{
+  const [isLoading, setIsLoading] = useState(true);
+  const [customers, setCustomers] = useState([]);
+  const [devices, setDevices] = useState([]);
+  const [ connection, setConnection ] = useState(null);
+  const latestDevices = useRef(null);
 
-class Monitoring extends Component {
-  state = {
-    isLoading: true,
-    customers: [],
-    devices:[]
-};
+  latestDevices.current = devices;
 
-  componentDidMount() {
-    userService.getMonitoringMap().then(response => {
-      const data = response.data;
-      console.log("data:", data)
-      if (data !== null) {
-        this.setState({ customers: data.customers, devices: data.devices});
-        this.setState({isLoading:false})
-      }
-    })
-    .catch(error => {
-      console.log("error", error);
-    });
+  useEffect(() => {
+    const newConnection = new HubConnectionBuilder()
+        .withUrl("http://notifier.dm1.com/Hubs/RealTimeHub", {
+          Headers:{'AppToken': 'RealTimeHubToken'}   
+        })
+        .withAutomaticReconnect()
+        .build();
+
+    setConnection(newConnection);
+}, []);
+
+useEffect(() => {
+  if (connection) {
+      connection.start()
+          .then(result => {
+              console.log('Connected!');
+
+              connection.on('NotifyAsync', message => {
+                  const updatedDevices = [...latestDevices.current];
+                  console.log('message', message);
+                  updatedDevices.push(message);
+              
+                  setDevices(updatedDevices);
+              });
+          })
+          .catch(e => console.log('Connection failed: ', e));
   }
-  handleBackToList = () => {
-    this.props.history.replace("/");
-  };
-  render(){
-    if(!this.state.isLoading){
-      return (
-        <Cluster customers={this.state.customers} devices={this.state.devices} onClick={this.handleBackToList}/>
-      );
-    }
-    return null;
-  }
+}, [connection]);
+  // useEffect(() => {
+
+  //   async function fetchData() {
+  //     await userService.getMonitoringMap().then(response => {
+  //       const data = response.data;
+  //       console.log("data:", data)
+  //       if (data !== null) {
+  //         setCustomers(data.customers);
+  //         setDevices(data.devices);
+  //         setIsLoading(false);
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.log("error", error);
+  //     });
+  //   }
+  //   fetchData();
+  // }, []);
+
+ 
+
+  // if(!isLoading){
+  //   return (
+  //     <Cluster customers={customers} devices={devices}/>
+  //   );
+  // }
+  return null;
 }
-
 export default Monitoring;
