a31485dbad5801f67e1f4df88d1f4bf1f638660c
dm1\u12797 <z.ghavidel@dm1.com>
2020-11-14 08:59:14 +0330

    Fix back button.

--- ClientApp/src/components/fabBackButton.jsx
+++ ClientApp/src/components/fabBackButton.jsx
@@ -3,7 +3,7 @@ import Fab from '@material-ui/core/Fab';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 const FabBackButton = (props) => {
-    return ( <Fab color="primary" aria-label="add" style={{zIndex:"1111", bottom: "88px",
+    return ( <Fab color="primary" aria-label="add" style={{zIndex:"1111", bottom: "18px",
     left: "18px", position: "absolute"}} onClick={()=>props.onClick()}>
        <ArrowBackIcon />
   </Fab> );
