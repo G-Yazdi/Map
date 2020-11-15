180240235ff5d511e7af2c42006fc385593e46f1
Hessam Hoseini <hessam8008@hotmail.com>
2020-11-15 11:06:09 +0330

    Update popup time in cluster.jsx

--- ClientApp/src/pages/monitoring/cluster.jsx
+++ ClientApp/src/pages/monitoring/cluster.jsx
@@ -2,73 +2,80 @@ import React from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 import Typography from '@material-ui/core/Typography';
-import {iconVisitor, iconMarket} from "../../components/icon";
+import { iconVisitor, iconMarket } from "../../components/icon";
 import Moment from "moment-jalali";
 
 export default function Cluster(props) {
-  const {customers, devices} = props;
+    const { customers, devices } = props;
 
-  return (
-    <React.Fragment>
-      <Map
-        className="markercluster-map"
-        center={[36.260464, 59.616756]}
-        zoom={10}
-        maxZoom={18}
-        style={{height:"92.64%",
-        position: "absolute",
-        bottom: 0,
-        width: "100%",
-        zIndex: "-1",
-        outline: "none"}}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {devices.map(item=>{
-            if(item.locationLatitude && item.locationLongitude){
-                console.log("date", new Date(item.locationTime));
-                console.log("date1", new Date());
-              return <Marker key={item.deviceId} position={[item.locationLatitude, item.locationLongitude]} icon={ iconVisitor }>
-                      <Popup className="leaflet">
-                        <Typography align="center" style={{fontFamily:"Vazir", fontSize: "13px",
-                              lineHeight: "30px"}}>
-                              نام: &nbsp; 
-                              {item.deviceNickname === "N/A"? "نامشخص": item.deviceNickname}
-                              <br/>
-                              IMEI: 
+    return (
+        <React.Fragment>
+            <Map
+                className="markercluster-map"
+                center={[36.260464, 59.616756]}
+                zoom={10}
+                maxZoom={18}
+                style={{
+                    height: "92.64%",
+                    position: "absolute",
+                    bottom: 0,
+                    width: "100%",
+                    zIndex: "-1",
+                    outline: "none"
+                }}
+            >
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                {devices.map(item => {
+                    if (item.locationLatitude && item.locationLongitude) {
+                        console.log("date", new Date(item.locationTime));
+                        console.log("date1", new Date());
+                        return <Marker key={item.deviceId} position={[item.locationLatitude, item.locationLongitude]} icon={iconVisitor}>
+                            <Popup className="leaflet">
+                                <Typography align="center" style={{
+                                    fontFamily: "Vazir", fontSize: "13px",
+                                    lineHeight: "30px"
+                                }}>
+                                    نام: &nbsp;
+                              {item.deviceNickname === "N/A" ? "نامشخص" : item.deviceNickname}
+                                    <br />
+                              IMEI:
                               {item.deviceIMEI}
-                              <br/>
-                              تاریخ:
-                              {` ${new Date(item.locationTime).toLocaleDateString('fa-IR') == 
-                                  new Date().toLocaleDateString('fa-IR')? "امروز": new Date(item.locationTime).toLocaleDateString('fa-IR')}`}
-                              <br/>
-                              زمان: 
-                              {` ${Moment(item.locationTime).format("HH:mm")}`}
-                        </Typography>
-                </Popup>
-              </Marker>;
-            }
-            else return null;
-        })}
-        <MarkerClusterGroup>
-            {customers.map(item=>
-            <Marker key={item.id} position={[item.locationLatitude, item.locationLongitude]} icon={ iconMarket }>
-                <Popup className="leaflet">
-                  <Typography align="right" style={{fontFamily:"Vazir", fontSize: "13px",
-                        lineHeight: "30px"}}>
-                        نام: &nbsp; 
+                                </Typography>
+                                <Typography align="left" style={{
+                                    fontFamily: "Vazir", fontSize: "11px", fontWeight:"bold",
+                                    lineHeight: "20px"}}>
+
+                                    {` ${new Date(item.locationTime).toLocaleDateString('fa-IR') ==
+                                        new Date().toLocaleDateString('fa-IR') ? "" : new Date(item.locationTime).toLocaleDateString('fa-IR') + "-"}`}
+                                    {` ${Moment(item.locationTime).format("HH:mm")}`}
+                                </Typography>
+                            </Popup>
+                        </Marker>;
+                    }
+                    else return null;
+                })}
+                <MarkerClusterGroup>
+                    {customers.map(item =>
+                        <Marker key={item.id} position={[item.locationLatitude, item.locationLongitude]} icon={iconMarket}>
+                            <Popup className="leaflet">
+                                <Typography align="right" style={{
+                                    fontFamily: "Vazir", fontSize: "13px",
+                                    lineHeight: "30px"
+                                }}>
+                                    نام: &nbsp;
                         {item.name}
-                        <br/>
-                        آدرس: &nbsp; 
+                                    <br />
+                        آدرس: &nbsp;
                         {item.address}
-                  </Typography>
-                </Popup>
-            </Marker>)}
-        </MarkerClusterGroup>
-      </Map>
-    
-    </React.Fragment>
+                                </Typography>
+                            </Popup>
+                        </Marker>)}
+                </MarkerClusterGroup>
+            </Map>
+
+        </React.Fragment>
     );
 }
